9b1571f098462101316d20217bba59a4d8ab6ae9
AdamMomen <adammuman81@gmail.com>
2020-01-07 14:55:09 +0100

    re-created react app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import React from 'react';
-import { SignUp } from './components/signup';
-import { Login } from './components/login';
-import { Router, Route, Switch, BrowserRouter } from "react-router";
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Welcome to React Router Tutorial</h2>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <ul className="navbar-nav mr-auto">
-          <li><Link to={'/'} className="nav-link"> Home </Link></li>
-          <li><Link to={'/signup'} className="nav-link">signup</Link></li>
-          <li><Link to={'/login'} className="nav-link">login</Link></li>
-        </ul>
-      </nav>
-      <hr />
-      {/* <Switch>
-          <Route exact path='/' component={} />
-          <Route path='/signup' component={SignUp} />
-          <Route path='/login' component={login} />
-        </Switch> */}
-      {/* <SignUp />
-        <Login /> */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
 export default App;
