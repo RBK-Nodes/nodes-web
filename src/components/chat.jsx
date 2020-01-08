1a44c10256e273acaefeb0dda9fcab713dcf0f50
AdamMomen <adammuman81@gmail.com>
2020-01-08 17:09:49 +0100

    implemting react hooks

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,44 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import io from 'socket.io-client';
 import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input'
+
+
+//set room name id here !!!
+
+useEffect(() => {
+    var soket = io('http://localhost:5000/room1').on('connect', () => {
+        console.log('connected!')
+    })
+})
+
 export function Chat(props) {
     //using hooks
     const [message, setMessage] = useState('')
-    var socketHandler = () => {
-        //set room name id here !!!
-        io('http://localhost:5000/room1').on('connect', () => {
-            console.log('connected!')
-        })
-    }
+
 
     var handleSubmit = (e) => {
         e.preventDefault()
-        //submit the form 
 
+        //submit the form 
+        setMessage('');
+        //here I want to use 
+        io.emit(message)
     }
     return (
         <div >
-
-
             THIS is the CHAT SECTION
             <br />
             <br />
             <br />
-            <div className="message-container"></div>
-            <div style={{ padding: "300px" }}>
-                <Input
-                    type="text"
-                    value={message}
-                    onChange={e => setMessage(e.target.value)}
-                    multiline></Input>
-                <Button
-                    onClick={e => {
-                        //call the Handlsubmit 
-                        setMessage('')
-                        console.log(message)
-                    }}
-                    color="secondary">Send</Button>
+            <div className="message-container">
+                <form
+                    onSubmit={handleSubmit}
+                    style={{ padding: "300px" }}>
+                    <Input
+                        type="text"
+                        value={message}
+                        onChange={e => setMessage(e.target.value)}
+                        multiline></Input>
+                    <Button
+                        type="submit"
+                        color="secondary"
+                    >Send</Button>
+                </form>
             </div>
         </div >
     )
