f8cc1b4e1c12f859fc7d4a4e03454a892b75ddcd
AdamMomen <adammuman81@gmail.com>
2020-01-09 15:39:16 +0100

    inhanced the visuals

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div >
-          <h2>Welcome to CHAT APP</h2>
+        <div>
+
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <ul className="navbar-nav mr-auto">
-              <li><Link to={'/login'} className="nav-link"> Login </Link></li>
-              <li><Link to={'/signup'} className="nav-link"> Signup </Link></li>
-              <li><Link to={'/home'} className="nav-link"> Home </Link></li>
-
+              <ul><Link to={'/login'} className="nav-link"> Login </Link></ul>
+              <ul><Link to={'/signup'} className="nav-link"> Signup </Link></ul>
+              <ul><Link to={'/home'} className="nav-link"> Home </Link></ul>
+              <h2>Welcome to CHAT APP</h2>
             </ul>
           </nav>
           <hr />
@@ -26,6 +26,7 @@ class App extends Component {
             <Route exact path='/login' component={Login} />
             <Route path='/signup' component={SignUp} />
             <Route path='/home' component={Home} />
+
           </Switch>
         </div>
       </Router>
--- src/components/friends/friendsList/friendsList.jsx
+++ src/components/friends/friendsList/friendsList.jsx
@@ -5,7 +5,7 @@ import Input from '@material-ui/core/Input'
 class FriendsList extends React.Component {
     render() {
         return (
-            <div className="rooms-list">
+            <div className="friends-list">
                 <form>
                     <Input
                         type="text"
