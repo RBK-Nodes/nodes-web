2c43c6c1b445936203b969eb467f0a9af56bd545
Hamam-Elmuratdh <hamam.alsaiity@gmail>
2020-01-09 17:20:35 +0100

    show request component complete

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
-import React, { Component } from 'react';
-import { Login } from './components/login/login.jsx'
-import { SignUp } from './components/signup/signup.jsx'
-import { Home } from './components/home/home.jsx'
-import './App.css';
+import React, { Component } from "react";
+import { Login } from "./components/login/login.jsx";
+import { SignUp } from "./components/signup/signup.jsx";
+import { Home } from "./components/home/home.jsx";
+import "./App.css";
+import "./components/request/request.css";
+import Request from "./components/request/request.jsx";
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 class App extends Component {
-
   render() {
     return (
       <Router>
-        <div >
+        <div>
           <h2>Welcome to CHAT APP</h2>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <ul className="navbar-nav mr-auto">
-              <li><Link to={'/login'} className="nav-link"> Login </Link></li>
-              <li><Link to={'/signup'} className="nav-link"> Signup </Link></li>
-              <li><Link to={'/home'} className="nav-link"> Home </Link></li>
-
+              <li>
+                <Link to={"/login"} className="nav-link">
+                  {" "}
+                  Login{" "}
+                </Link>
+              </li>
+              <li>
+                <Link to={"/signup"} className="nav-link">
+                  {" "}
+                  Signup{" "}
+                </Link>
+              </li>
+              <li>
+                <Link to={"/home"} className="nav-link">
+                  {" "}
+                  Home{" "}
+                </Link>
+              </li>
             </ul>
           </nav>
           <hr />
+          <Request />
           <Switch>
-
-            <Route exact path='/login' component={Login} />
-            <Route path='/signup' component={SignUp} />
-            <Route path='/home' component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/home" component={Home} />
           </Switch>
         </div>
       </Router>
     );
   }
 }
-
 
 export default App;
---
+++ src/components/request/request.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+function Request() {
+  const [request, setRequest] = useState("");
+  return <div className="freind"> Request</div>;
+}
+
+export default Request;
