aa350290e6aad75643b97b529e10966d115fbfd0
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2020-01-10 20:35:14 +0100

    bug fixing requestList file

--- src/components/friends/firendRequest/requestsList.jsx
+++ src/components/friends/firendRequest/requestsList.jsx
@@ -1,21 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Request from './request.jsx'
 import { getAllRequests } from '../../../chat_controller/controller.js'
-var request;
-getAllRequests(localStorage.getItem("username"))
-    .then(result => {
-        request = result.data
-    })
-function FriendRequestList() {
-    const [friendRequests, setFriendRequests] = useState(
-        [
-            "Adam",
-            "Mohammed",
-            "Abdo"
-        ]
-    )
-    // I need to edit this later
 
+function FriendRequestList(props) {
+    const [friendRequests, setFriendRequests] = useState([]);
+    const [searched, setSearched] = useState(false)
+    // I need to edit this later
+    if(!searched) {
+        getAllRequests(localStorage.getItem("username"))
+    .then(result => {
+        console.log("data here",result.data)
+        setSearched(true)
+        setFriendRequests(result.data)
+
+    })
+    .catch(err=>{
+        console.log('error', err)
+    })
+    }
 
     return (
         <div className="requests"
@@ -25,7 +27,7 @@ function FriendRequestList() {
         >
             {
 
-                request.map((friend, index) => {
+                friendRequests.map((friend, index) => {
                     return <Request req={friendRequests} key={index} id={index} name={friend} />;
                 })
             }
