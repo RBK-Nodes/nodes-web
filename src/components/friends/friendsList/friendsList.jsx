4dc78944f68c3328d44825b48d85bef318f7de66
AdamMomen <adammuman81@gmail.com>
2020-01-11 21:35:57 +0100

    merged

--- src/components/friends/friendsList/friendsList.jsx
+++ src/components/friends/friendsList/friendsList.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Button from '@material-ui/core/Button';
 import { getAllFriends } from '../../../chat_controller/controller';
-import Friend from './friend.jsx'
 function FriendsList(props) {
     const [friends, setFriends] = useState([])
 
